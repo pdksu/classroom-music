6c8431626fb79feda302ec461e41c774ffe9e57f
Peter Kaplan <peter.kaplan@gmail.com>
2022-08-18 09:10:21 -0400

    set for py3.8

--- classroom-music.py
+++ classroom-music.py
@@ -1,6 +1,6 @@
-# -*- coding: future_fstrings -*-
 import csv, sqlite3, yaml
-from time import strptime
+from datetime import datetime as dt
+from time import strftime, strptime
 from typing import OrderedDict
 from pathlib import Path
 
@@ -37,14 +37,14 @@ def csv_to_sql(fname : Path, cur : sqlite3.Cursor, table : str ):
                 create_command = f"CREATE TABLE {table} ("
                 dtypes = {}
                 for k in r.fieldnames:
-                    for dt,f in SQLtypes.items():
+                    for Dt, f in SQLtypes.items():
                         try:
                             vout = f(row[k])
                             break
                         except ValueError:
                             pass
-                    dtypes[k] = dt
-                    create_command += f"{k}  {dt}, "
+                    dtypes[k] = Dt
+                    create_command += f"{k}  {Dt}, "
                 create_command = create_command[0:-2] + ");"
                 cur.execute(create_command)
                 db_result = cur.fetchall()
@@ -73,21 +73,48 @@ class Sched_db:
             n=csv_to_sql(Path(self.dir,f), cur, t)
             print(f'{t} has {n} rows')
     
+    def getDefaultScript(self):
+        with open(self.script, 'r') as f:
+            queryText = ''.join(f.readlines())
+        return queryText
+
+    
     def dotest(self):
         self.cursor.execute('SELECT COUNT(*) FROM calendar')
         n = self.cursor.fetchone()
         print(f'found {n[0]} rows in calendar')
         with open(self.script, 'r') as f:
             queryText = ''.join(f.readlines())
+        queryText = queryText.replace("REPDATE","9/8/2022")
         print(queryText)
         self.cursor.execute(queryText)
         rows = self.cursor.fetchall()
         for v in rows:
             print(f'OUT: {v}')
-
+    
+    def dayBells(self, date):
+        queryText = self.getDefaultScript()
+        queryText = queryText.replace("REPDATE",date)
+        self.cursor.execute(queryText)
+        rows = self.cursor.fetchall()
+        rows = [{'date':row[0], 'classTime':row[1], 'classDismissTime':row[2],
+                 'offset':row[3],'end':bool(row[4]), 'signal':row[5], 'file':row[6],
+                 'period':row[7], 'cName':row[8], 'section':row[9],'lesson':row[10]} 
+                        for row in rows]
+        return(rows)
+    
+    def bellTime(self, bell):
+        bellDate = strftime(bell['date'],"%-m/%-d/%Y")
+        bellTime = ... [if else]
+    
 
 if __name__ == "__main__":
     con = sqlite3.connect(":memory:")
     cur = con.cursor()
     s = Sched_db(YAML, cur)
     s.dotest()
+    today = dt.strftime(dt.today(), "%-m/%-d/%Y")
+    today="10/10/2022"
+    bells = s.dayBells(today)
+    for bell in bells:
+        print(bell)
